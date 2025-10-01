96da8fba08ebedb1ab51b5afc57887b1d0a840f5
zhsxkc <erikams711@gmail.com>
2025-10-01 12:24:49 -0600

    Corrección de errores
    Se corrigió routes.ts

--- tortadelomo/src/app/app.routes.ts
+++ tortadelomo/src/app/app.routes.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './home/home';
 import { About } from './about/about';
-import { ProjectsComponent } from './projects/projects';
+import { Projects } from './projects/projects';
 import { Contact } from './contact/contact';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: Home },
   { path: 'about', component: About },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'contact', component: Contact }
+  { path: 'projects', component: Projects },
+  { path: 'contact', component: Contact },
+  { path: '**', redirectTo: '' } // ruta comodín
 ];
 
 @NgModule({
